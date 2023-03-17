93353b9ac2caae13eccfb4fcb07076480c7804d7
xiaomaotongzhi <2359643054@qq.com>
2023-03-17 20:38:22 +0800

    version-1.0

--- src/test/java/com/fuchuang/A33/SpringSecurityApplicationTests.java
+++ src/test/java/com/fuchuang/A33/SpringSecurityApplicationTests.java
@@ -3,12 +3,20 @@ package com.fuchuang.A33;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
 
     @Test
-    void test1(){
+    void test1() throws IOException {
     }
 
 }
