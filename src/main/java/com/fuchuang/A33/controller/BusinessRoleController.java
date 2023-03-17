8a87b7a75228876d30e6010b4bce74d0bf2020e2
xiaomaotongzhi <2359643054@qq.com>
2023-03-17 22:59:42 +0800

    version-1.0

--- src/main/java/com/fuchuang/A33/controller/BusinessRoleController.java
+++
@@ -1,9 +0,0 @@
-package com.fuchuang.A33.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/businessRole")
-public class BusinessRoleController {
-}
