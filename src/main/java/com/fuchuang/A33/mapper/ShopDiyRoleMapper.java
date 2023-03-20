6564c9db082a731307adcdd6854a5345c4ae8ba3
Goshic <2069762101@qq.com>
2023-03-20 21:43:42 +0800

    增加自定义规则

---
+++ src/main/java/com/fuchuang/A33/mapper/ShopDiyRoleMapper.java
@@ -0,0 +1,9 @@
+package com.fuchuang.A33.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fuchuang.A33.entity.ShopDiyRole;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShopDiyRoleMapper extends BaseMapper<ShopDiyRole> {
+}
